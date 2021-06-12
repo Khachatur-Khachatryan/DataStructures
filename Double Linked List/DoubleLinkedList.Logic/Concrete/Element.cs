c475321f48823ebb76d987cf5f79d80f409971e7
KHACHATUR228 <xachulxx@gmail.com>
2021-06-12 17:10:36 +0300

    double linked list

---
+++ Double Linked List/DoubleLinkedList.Logic/Concrete/Element.cs
@@ -0,0 +1,16 @@
+﻿namespace LinkedList.Logic
+{
+    public class Element<T> : IElement<T>
+    {
+        public T Data { get; set; }
+        public IElement<T> Previous { get; set; }
+        public IElement<T> Next { get; set; }
+
+        public Element(T data)
+        {
+            Data = data;
+        }
+
+        public Element() { }
+    }
+}
---
+++ LinkedList.Tests/UnitTest1.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using LinkedList.Logic;
+
+namespace LinkedList.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        [Test]
+        public void AddHeadTest()
+        {
+            // Arrange
+            var linkedList = new LinkedList<int>();
+
+            // Act 
+            linkedList.AddHead(41);
+            linkedList.AddHead(37);
+            linkedList.AddHead(24);
+
+            // Assert
+            linkedList.Head.Should().Equals(24);
+            linkedList.Tail.Should().Equals(41);
+            linkedList.Count.Should().Equals(3);
+            linkedList.IsEmpty.Should().BeFalse();
+        }
+
+        [Test]
+        public void AddTailTest()
+        {
+            // Arrange
+            var linkedList = new LinkedList<char>();
+
+            // Act
+            linkedList.AddTail('♂');
+            linkedList.AddTail('3');
+            linkedList.AddTail('}');
+
+            // Assert
+            linkedList.Head.Should().Equals('♂');
+            linkedList.Tail.Should().Equals('}');
+            linkedList.Count.Should().Equals(3);
+            linkedList.IsEmpty.Should().BeFalse();
+        }
+
+        [Test]
+        public void RemoveHeadTest()
+        {
+            // Arrange
+            var linkedList = new LinkedList<dynamic>();
+            linkedList.AddHead(13); 
+            linkedList.AddHead('$'); 
+            linkedList.AddHead("stalker top"); 
+            linkedList.AddHead(true);
+            linkedList.AddTail(0.24F);
+
+            // Act
+            linkedList.RemoveHead();
+            linkedList.RemoveHead();
+
+            // Assert
+            linkedList.Head.Should().Equals("stalker top");
+            linkedList.Tail.Should().Equals(0.24F);
+            linkedList.Count.Should().Equals(3);
+            linkedList.IsEmpty.Should().BeFalse();
+        }
+
+        [Test]
+        public void RemoveTailTest()
+        {
+            // Arrange
+            var linkedList = new LinkedList<int>();
+            linkedList.AddHead(13);
+            linkedList.AddTail(45);
+            linkedList.AddTail(98);
+            linkedList.AddTail(532);
+            linkedList.AddTail(35);
+
+            // Act
+            linkedList.RemoveTail();
+            linkedList.RemoveTail();
+
+            // Assert
+            linkedList.Head.Should().Equals(13);
+            linkedList.Tail.Should().Equals(98);
+            linkedList.Count.Should().Equals(3);
+            linkedList.IsEmpty.Should().BeFalse();
+        }
+    }
+}
